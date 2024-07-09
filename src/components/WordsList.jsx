1e4337c1131d1892f475a79508273a157dd47c16
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2024-07-09 17:59:09 +0300

    added react-speech-kit to listen the words

--- src/components/WordsList.jsx
+++ src/components/WordsList.jsx
@@ -1,14 +1,31 @@
 import {wordsData} from '../../db/dataBase'
+import React, { useCallback, useEffect, useState } from "react";
+import { useSpeechSynthesis } from 'react-speech-kit';
+
 
 const WordsList = () => {
+  const { speak, voices } = useSpeechSynthesis();
+
+  const rate = 0.7; // Speed
+  const pitch = 0.6;
+  const voice = voices[2] // Voice type
+
+  const handleListenWord = (item) => {
+    speak({ text: item.word, voice, rate, pitch })
+  }
+  const handleListenSentence = (item) => {
+    speak({ text: item.sentence, voice, rate, pitch })
+  }
+
+
   return (
     <>
       <ul>
         {
           wordsData.map(item => (
             <li>
-              {item.word} - {item.translation}<br/>
-              {item.sentence}
+              <span onClick={() => handleListenWord(item)}>{item.word}</span> - {item.translation}<br/>
+              <span onClick={() => handleListenSentence(item)}>{item.sentence}</span>
             </li>
           ))
         }
